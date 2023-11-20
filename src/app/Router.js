7ceb95b277454d5935c18423fc4e90392250b50f
Youssef Ashraf <139683775+YoussefAshraf001@users.noreply.github.com>
2023-11-20 10:19:02 +0200

    Updated Router.js
    fixed Module not found Error

--- src/app/Router.js
+++ src/app/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "../../node_modules/react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
 import "../App.css";
 import AnyPhoto from "../pages/AnyPhoto/AnyPhoto";
